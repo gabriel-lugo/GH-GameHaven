7267134a28f36c2b8058d65bc12741685e12f731
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-26 13:16:00 +0100

    Mantine styles + Burger implementation + Responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
-import { Burger, Container, Group, Title } from "@mantine/core";
+import {
+  Burger,
+  Container,
+  Group,
+  Paper,
+  Title,
+  Transition,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { useState } from "react";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../css/Header.css";
 
 const links = [
@@ -11,39 +19,62 @@ const links = [
 ];
 
 function Header() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const location = useLocation();
+  const isActive = (pathname: string) => location.pathname === pathname;
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        close();
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [close]);
 
   const items = links.map((link) => (
-    <a
+    <Link
       key={link.label}
-      href={link.link}
-      className="link"
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
+      to={link.link}
+      className={`link ${isActive(link.link) ? "link-active" : ""}`}
+      onClick={() => {
+        if (opened) {
+          close();
+        }
       }}
     >
       {link.label}
-    </a>
+    </Link>
   ));
+
   return (
-    <header>
-      <Container size="md" className="inner">
-        <Title order={2}>GH-GameHaven</Title>
-        <Group gap={5} visibleFrom="xs">
-          {items}
-        </Group>
-      </Container>
-      <Burger
-        color="#F2C341"
-        opened={opened}
-        onClick={toggle}
-        hiddenFrom="xs"
-        size="sm"
-      />
-    </header>
+    <>
+      <header>
+        <Container size="md" className="inner">
+          <Title order={2}>GH-GameHaven</Title>
+          <Group gap={5} className={`header-links ${opened ? "opened" : ""}`}>
+            {" "}
+            {items}
+          </Group>
+          <Burger
+            color="#F2C341"
+            opened={opened}
+            onClick={toggle}
+            className="burger"
+            size="md"
+          />
+        </Container>
+      </header>
+      <Transition transition="pop-top-right" duration={200} mounted={opened}>
+        {(styles) => (
+          <Paper className="dropdown" withBorder style={styles}>
+            {items}
+          </Paper>
+        )}
+      </Transition>
+    </>
   );
 }
 

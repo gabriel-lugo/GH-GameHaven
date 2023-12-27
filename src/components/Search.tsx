45864afadc57cc4624c934f6566723b81bc5a872
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-27 14:32:05 +0100

    search for different screen sizes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,20 @@
 import { Input } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { RxCrossCircled } from "react-icons/rx";
 import "../css/Search.css";
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   const [isInputVisible, setIsInputVisible] = useState(window.innerWidth > 768);
+  const [isScreenWidthSmall, setIsScreenWidthSmall] = useState(
+    window.innerWidth <= 768
+  );
 
   useEffect(() => {
     const handleResize = () => {
       setIsInputVisible(window.innerWidth > 768);
+      setIsScreenWidthSmall(window.innerWidth <= 768);
     };
 
     window.addEventListener("resize", handleResize);
@@ -24,12 +29,19 @@ function Search() {
     setIsInputVisible(!isInputVisible);
   };
 
+  const handleIconClick = (event: any) => {
+    event.stopPropagation();
+    toggleInputVisibility();
+  };
+
   return (
-    <div className="container-style">
-      {isInputVisible && (
+    <div className={`container-style ${isInputVisible ? "input-visible" : ""}`}>
+      {isInputVisible ? (
         <Input
+          onClick={handleIconClick}
+          leftSection={isScreenWidthSmall ? <RxCrossCircled /> : null}
           rightSection={
-            <div className="icon-wrapper" onClick={toggleInputVisibility}>
+            <div className="icon-wrapper">
               <IoIosSearch size={25} />
             </div>
           }
@@ -37,10 +49,8 @@ function Search() {
           placeholder="Search for a game.."
           value={searchTerm}
           onChange={handleInputChange}
-          className="input-style"
         />
-      )}
-      {!isInputVisible && (
+      ) : (
         <div className="icon-only" onClick={toggleInputVisibility}>
           <IoIosSearch size={25} />
         </div>

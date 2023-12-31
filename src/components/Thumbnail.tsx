85bf8a559f5ded0293e0318199d05bcc539edb32
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-31 02:12:07 +0100

    add rating + hover

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,21 +1,41 @@
-import { Box, Image, Title } from "@mantine/core";
+import { Box, Image, Text, Title } from '@mantine/core';
 // import { GiCrownedHeart } from "react-icons/gi";
-import { IoHeartOutline } from "react-icons/io5";
-import "../css/Thumbnail.css";
+import { IoHeartOutline } from 'react-icons/io5';
+import '../css/Thumbnail.css';
 
 interface Game {
   name: string;
   cover: string;
+  rating: number;
 }
 
 const Thumbnail: React.FC<{ game: Game }> = ({ game }) => {
+  const getRatingClass = (rating: number) => {
+    if (rating >= 75) {
+      return 'rating-high';
+    } else if (rating >= 50) {
+      return 'rating-medium';
+    } else {
+      return 'rating-low';
+    }
+  };
   return (
     // <Card radius="md" shadow="sm" className="card-container">
     <Box className="card-container">
-      <Image src={game.cover} alt={game.name} radius="md" />
+      <Box className="thumbnail-image">
+        <Image src={game.cover} alt={game.name} />
+        <Box className="hover-content">
+          <Title className="game-info" order={5}>
+            Game Info
+          </Title>
+        </Box>
+      </Box>
       {/* <GiCrownedHeart /> */}
       <IoHeartOutline className="heart-icon" />
       <Title order={5}>{game.name}</Title>
+      <Text className={`rating ${getRatingClass(game.rating)}`}>
+        {Math.round(game.rating)}
+      </Text>
     </Box>
     // </Card>
   );

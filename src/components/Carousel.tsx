a3d318f4ceadcc0524abe0764aab12932a290bae
Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
2024-01-03 13:46:02 +0100

    style arrows

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,7 @@ import SwiperCore from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
+import { FreeMode, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../css/Carousel.css";
 import { Game } from "../pages/HomePage";
@@ -13,16 +13,19 @@ interface CarouselProps {
   games: Game[];
 }
 
-SwiperCore.use([Navigation]);
+SwiperCore.use([FreeMode, Navigation]);
 
 function Carousel({ games }: CarouselProps) {
   return (
     <Box mt={"lg"} mb={"lg"}>
       <Swiper
         slidesPerView={"auto"}
+        freeMode={true}
         spaceBetween={16}
         className="carousel-container"
+        modules={[FreeMode, Navigation]}
         navigation={true}
+        watchOverflow={true}
         loop={true}
       >
         {games.map((game) => (

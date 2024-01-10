d198aae9f1b55e6430fbf49794c586943e7f2a65
Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
2024-01-10 08:46:56 +0100

    add carousel of upcoming games

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
 import { Title } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { getNewGames, getTopRatedGames } from "../api/igdbApi";
+import {
+  getNewGames,
+  getTopRatedGames,
+  getUpcomingGames,
+} from "../api/igdbApi";
 import Carousel from "../components/Carousel";
 import GhInfo from "../components/GhInfo";
 import HeroSlide from "../components/HeroSlide";
@@ -20,6 +24,7 @@ export interface Game {
 function HomePage() {
   const [topRatedGames, setTopRatedGames] = useState<Game[]>([]);
   const [newestGames, setNewestGames] = useState<Game[]>([]);
+  const [upcomingGames, setUpcomingGames] = useState<Game[]>([]);
 
   // useEffect(() => {
   //   const query = "Hades"; // Replace with the desired game name
@@ -62,6 +67,19 @@ function HomePage() {
       });
   }, []);
 
+  useEffect(() => {
+    getUpcomingGames("playstation")
+      .then((upcomingGames) => {
+        // Log the newest games information to the console
+        console.log("Newest Games:", upcomingGames);
+        setUpcomingGames(upcomingGames);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <>
       <HeroSlide games={topRatedGames} />
@@ -79,6 +97,10 @@ function HomePage() {
         Top Rated Games
       </Title>
       <Carousel games={topRatedGames} />
+      <Title order={2} pl={"md"} mb={"md"}>
+        Newest Games
+      </Title>
+      <Carousel games={newestGames} />
       <GhInfo />
       {/* <Container size={"xl"}>
         <Title order={2} mb={"md"}>
@@ -91,9 +113,9 @@ function HomePage() {
         </SimpleGrid>
       </Container> */}
       <Title order={2} pl={"md"} mb={"md"}>
-        Newest Games
+        Upcoming Games
       </Title>
-      <Carousel games={newestGames} />
+      <Carousel games={upcomingGames} />
     </>
   );
 }

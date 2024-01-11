ce9bb0fd62e4f30be0219de7b118606c59324856
Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
2024-01-11 14:18:12 +0100

    add profilepage

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,57 @@
-import { Title } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Container,
+  Image,
+  Paper,
+  PasswordInput,
+  Stack,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import logo from "../assets/GH-logo.png";
+import "../css/ProfilePage.css";
 
 function ProfilePage() {
-  return <Title order={2}>Profile</Title>;
+  return (
+    <Container size={"md"}>
+      <Paper
+        className="profile-settings-paper"
+        style={{ background: "#f9f6ee" }}
+        radius="md"
+        p="xl"
+        shadow="md"
+        mt={"xl"}
+        mb={"lg"}
+        withBorder
+      >
+        <Box className="profile-settings-wrapper">
+          <Box className="profile-settings-image">
+            <Title order={3}>Username</Title>
+            <Image src={logo} w={200} />
+            <Button className="button-style">Change Profile Image</Button>
+          </Box>
+          <Box className="profile-settings-form">
+            <form>
+              <Stack>
+                <TextInput label="Email" disabled />
+                <TextInput label="Username" />
+                <PasswordInput label="New Password" placeholder="●●●●●●●●●●" />
+                <PasswordInput
+                  required
+                  label="Current Password"
+                  placeholder="●●●●●●●●●●"
+                />
+                <Button className="button-style" fullWidth>
+                  Save Changes
+                </Button>
+              </Stack>
+            </form>
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
 
 export default ProfilePage;

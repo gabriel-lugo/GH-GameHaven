85815d7bc18ac56cbf7a92e3385f30b766ae3c02
Hampus Isebring <hampus.isebring@gmail.com>
2024-01-13 22:41:11 +0100

    add fallback when no favorites available

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Button,
   Container,
-  Flex,
   Group,
   Image,
   Paper,
@@ -36,91 +35,89 @@ function ContactPage() {
   });
 
   return (
-    <Flex style={{ height: "100dvh" }} justify="center" align="center">
-      <Container mt="xl">
-        <Paper shadow="md" radius="lg">
-          <Box className="wrapper">
-            <Box className="contacts">
-              <Text fz="lg" fw={700} className="title" c="#262626">
-                Reach Out
+    <Container style={{ marginTop: "10rem" }}>
+      <Paper shadow="md" radius="lg">
+        <Box className="wrapper">
+          <Box className="contacts">
+            <Text fz="lg" fw={700} className="title" c="#262626">
+              Reach Out
+            </Text>
+            <Box className="social-contact">
+              <MdOutlineAlternateEmail style={{ fontSize: "1.2rem" }} />
+              <Text size="sm">
+                <a
+                  style={{ textDecoration: "none", color: "#262626" }}
+                  href="mailto:contact@ghgamehaven.com"
+                >
+                  contact@ghgamehaven.com
+                </a>
               </Text>
-              <Box className="social-contact">
-                <MdOutlineAlternateEmail style={{ fontSize: "1.2rem" }} />
-                <Text size="sm">
-                  <a
-                    style={{ textDecoration: "none", color: "#262626" }}
-                    href="mailto:contact@ghgamehaven.com"
-                  >
-                    contact@ghgamehaven.com
-                  </a>
-                </Text>
-              </Box>
-              <Box className="social-contact" mt="md">
-                <GrLocation style={{ fontSize: "1.2rem" }} />
-                <Text size="sm">
-                  <a
-                    style={{ textDecoration: "none", color: "#262626" }}
-                    href="https://www.google.com/maps/place/Medieinstitutet/@57.7097458,11.9919449,17z/data=!3m1!4b1!4m6!3m5!1s0x464ff30de9cbee3d:0xbb3f902b63916df6!8m2!3d57.7097458!4d11.9945252!16s%2Fg%2F11ghpmkkpk?entry=ttu"
-                    target="_blank"
-                  >
-                    Anders Personsgatan 18, <br />
-                    416 64 Gothenburg, Sweden
-                  </a>
-                </Text>
-              </Box>
-              <Image
-                src={contactUs}
-                alt="GH Mascot welcoming you to contact Gamehaven"
-              />
             </Box>
-
-            <form className="form" onSubmit={form.onSubmit(console.log)}>
-              <Text fz="lg" fw={700} className="title">
-                Get in touch
+            <Box className="social-contact" mt="md">
+              <GrLocation style={{ fontSize: "1.2rem" }} />
+              <Text size="sm">
+                <a
+                  style={{ textDecoration: "none", color: "#262626" }}
+                  href="https://www.google.com/maps/place/Medieinstitutet/@57.7097458,11.9919449,17z/data=!3m1!4b1!4m6!3m5!1s0x464ff30de9cbee3d:0xbb3f902b63916df6!8m2!3d57.7097458!4d11.9945252!16s%2Fg%2F11ghpmkkpk?entry=ttu"
+                  target="_blank"
+                >
+                  Anders Personsgatan 18, <br />
+                  416 64 Gothenburg, Sweden
+                </a>
               </Text>
-
-              <Box className="fields">
-                <SimpleGrid cols={{ base: 1, sm: 2 }}>
-                  <TextInput
-                    label="Your name"
-                    placeholder="Your name"
-                    {...form.getInputProps("name")}
-                  />
-                  <TextInput
-                    required
-                    label="Your email"
-                    placeholder="your@mail.com"
-                    {...form.getInputProps("email")}
-                  />
-                </SimpleGrid>
-
-                <TextInput
-                  mt="md"
-                  label="Subject"
-                  placeholder="Subject"
-                  {...form.getInputProps("subject")}
-                />
-
-                <Textarea
-                  required
-                  mt="md"
-                  label="Your message"
-                  placeholder="Please include all relevant information"
-                  minRows={3}
-                  {...form.getInputProps("message")}
-                />
-
-                <Group justify="flex-end" mt="md">
-                  <Button type="submit" className="control">
-                    Send message
-                  </Button>
-                </Group>
-              </Box>
-            </form>
+            </Box>
+            <Image
+              src={contactUs}
+              alt="GH Mascot welcoming you to contact Gamehaven"
+            />
           </Box>
-        </Paper>
-      </Container>
-    </Flex>
+
+          <form className="form" onSubmit={form.onSubmit(console.log)}>
+            <Text fz="lg" fw={700} className="title">
+              Get in touch
+            </Text>
+
+            <Box className="fields">
+              <SimpleGrid cols={{ base: 1, sm: 2 }}>
+                <TextInput
+                  label="Your name"
+                  placeholder="Your name"
+                  {...form.getInputProps("name")}
+                />
+                <TextInput
+                  required
+                  label="Your email"
+                  placeholder="your@mail.com"
+                  {...form.getInputProps("email")}
+                />
+              </SimpleGrid>
+
+              <TextInput
+                mt="md"
+                label="Subject"
+                placeholder="Subject"
+                {...form.getInputProps("subject")}
+              />
+
+              <Textarea
+                required
+                mt="md"
+                label="Your message"
+                placeholder="Please include all relevant information"
+                minRows={3}
+                {...form.getInputProps("message")}
+              />
+
+              <Group justify="flex-end" mt="md">
+                <Button type="submit" className="control">
+                  Send message
+                </Button>
+              </Group>
+            </Box>
+          </form>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
 

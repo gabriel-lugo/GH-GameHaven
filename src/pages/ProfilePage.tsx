8d4b334cf8b42c38767d9f24ba81fd18434b1936
Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
2024-01-15 15:17:35 +0100

    update profileimage, fixed mantine validation

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -19,18 +19,17 @@ import {
   updatePassword,
   updateProfile,
 } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { MdOutlineError, MdThumbUp } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/GH-logo.png";
-import profile1thumb from "../assets/profile1thumb.jpg";
+import profile1 from "../assets/profile1.jpg";
 import "../css/ProfilePage.css";
 import { auth, db } from "../firebase";
-import profile2thumb from "./../assets/profile2thumb.jpg";
+import profile2 from "./../assets/profile2.jpg";
 
 interface FormValues {
-  email: string;
   name: string;
   newPassword: string;
   currentPassword: string;
@@ -38,26 +37,33 @@ interface FormValues {
 
 function ProfilePage() {
   const [user, setUser] = useState<User | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [formValues, setFormValues] = useState<FormValues>({
-    email: "",
-    name: "",
-    newPassword: "",
-    currentPassword: "",
-  });
+  const [selectedProfileImage, setSelectedProfileImage] = useState<number>(1);
+  const [error, setError] = useState<string>("");
+
+  const profileImages = [logo, profile1, profile2];
 
   const form = useForm<FormValues>({
-    initialValues: formValues,
+    initialValues: {
+      // email: "",
+      name: "",
+      newPassword: "",
+      currentPassword: "",
+    },
 
     validate: {
       name: (val) =>
-        val.length < 2 ? "Username should include at least 2 characters" : null,
-      email: (val) =>
-        /^\S+@\S+$/.test(val)
-          ? null
-          : "Make sure you are using mail@mail.com format",
-      newPassword: (val) =>
-        val.length < 7 ? "Password should include at least 7 characters" : null,
+        val.length <= 1
+          ? "Username should include at least 2 characters"
+          : null,
+      newPassword: (val) => {
+        if (form.values.newPassword.trim() === "") {
+          return null;
+        }
+
+        return val.length <= 6
+          ? "Password should include at least 7 characters"
+          : null;
+      },
       currentPassword: (val) =>
         val.length === 0 ? "Please enter your current password." : null,
     },
@@ -66,24 +72,32 @@ function ProfilePage() {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser: User | null) => {
       setUser(authUser);
-      if (authUser && authUser.displayName) {
-        setFormValues({
-          email: authUser.email || "",
-          name: authUser.displayName,
-          newPassword: "",
-          currentPassword: "",
-        });
-      }
     });
     return () => unsubscribe();
   }, []);
 
-  const handleSaveChanges = async () => {
-    setError(null);
+  useEffect(() => {
+    const fetchProfileImageId = async () => {
+      try {
+        if (user) {
+          const userRef = doc(db, "users", user.uid);
+          const userSnapshot = await getDoc(userRef);
+          const userData = userSnapshot.data();
+          if (userData && userData.profileImageId !== undefined) {
+            setSelectedProfileImage(userData.profileImageId - 1);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching profile image id:", error);
+      }
+    };
 
+    fetchProfileImageId();
+  }, [user]);
+
+  const onSubmit = async () => {
     try {
       if (!form.values.currentPassword) {
-        setError("Please enter your current password.");
         return;
       }
 
@@ -98,18 +112,23 @@ function ProfilePage() {
 
         console.log("Reauthentication successful. Updating profile...");
 
-        await updateProfile(user, {
-          displayName: form.values.name || user.displayName,
-        });
+        if (form.values.name.length < 2) {
+          return;
+        }
 
-        console.log("Updated display name:", form.values.name);
+        await updateProfile(user, {
+          displayName: form.values.name,
+        });
 
         const userRef = doc(db, "users", user.uid);
         await updateDoc(userRef, {
           username: form.values.name,
         });
 
-        console.log("Updated username:", form.values.name);
+        console.log(
+          "Updated display name and Firestore document:",
+          form.values.name
+        );
 
         if (form.values.newPassword) {
           console.log("Updating password...");
@@ -129,7 +148,6 @@ function ProfilePage() {
         console.log("Profile updated successfully!");
       }
     } catch (error) {
-      setError("Failed to update profile. Please check your current password");
       showNotification({
         title: "Failed to update profile!",
         message: "Please check your current password.",
@@ -140,9 +158,39 @@ function ProfilePage() {
     }
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    handleSaveChanges();
+  const handleSaveProfileImage = async () => {
+    try {
+      if (user) {
+        console.log("Updating profile image...");
+
+        const userRef = doc(db, "users", user.uid);
+        await updateDoc(userRef, {
+          profileImageId: selectedProfileImage + 1,
+        });
+
+        showNotification({
+          title: "Profile image updated successfully!",
+          message: "Your profile image has been updated successfully.",
+          color: "green",
+          icon: <MdThumbUp />,
+        });
+
+        console.log("Profile image updated successfully!");
+      }
+    } catch (error) {
+      setError("Failed to update profile image. Please try again.");
+      showNotification({
+        title: "Failed to update profile image!",
+        message: "Please try again.",
+        color: "red",
+        icon: <MdOutlineError />,
+      });
+      console.error(error);
+    }
+  };
+
+  const handleSelectProfileImage = (index: number) => {
+    setSelectedProfileImage(index);
   };
 
   if (!user) {
@@ -171,27 +219,35 @@ function ProfilePage() {
         <Box className="profile-settings-wrapper">
           <Box className="profile-settings-image">
             <Title order={3}>{user.displayName || "Username"}</Title>
-            <Image src={logo} w={200} />
+            <Image src={profileImages[selectedProfileImage]} w={200} />
             <Flex>
-              <Image src={logo} w={102} />
-              <Image src={profile1thumb} w={102} />
-              <Image src={profile2thumb} w={102} />
+              {profileImages.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  w={80}
+                  onClick={() => handleSelectProfileImage(index)}
+                />
+              ))}
             </Flex>
-            <Button className="button-style">Change Profile Image</Button>
+            <Button className="button-style" onClick={handleSaveProfileImage}>
+              Save Profile Image
+            </Button>
+            {error && <div style={{ color: "red" }}>{error}</div>}
           </Box>
           <Box className="profile-settings-form">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={form.onSubmit(onSubmit)}>
               <Stack>
                 <TextInput label="Email" value={user?.email || ""} disabled />
                 <TextInput
                   label="Username"
+                  name="name"
                   placeholder={user?.displayName || "Your name"}
                   value={form.values.name}
                   onChange={(event) =>
                     form.setFieldValue("name", event.currentTarget.value)
                   }
                   error={form.errors.name}
-                  radius="md"
                 />
 
                 <PasswordInput
@@ -207,7 +263,7 @@ function ProfilePage() {
                 <PasswordInput
                   label="Current Password"
                   placeholder="●●●●●●●●●●"
-                  required
+                  withAsterisk
                   value={form.values.currentPassword}
                   onChange={(event) =>
                     form.setFieldValue(
@@ -224,7 +280,6 @@ function ProfilePage() {
                 </Button>
               </Stack>
             </form>
-            {error && <div style={{ color: "red" }}>{error}</div>}
           </Box>
         </Box>
       </Paper>

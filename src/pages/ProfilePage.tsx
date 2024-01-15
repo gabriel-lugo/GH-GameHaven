4e4c08b57efbb8bcdfc55cf0e98336064d79d8cd
Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
2024-01-15 11:42:08 +0100

    add validation, user can change username and password, add notifications

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -9,41 +9,71 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { showNotification } from "@mantine/notifications";
 import {
   EmailAuthProvider,
   User,
   reauthenticateWithCredential,
+  updatePassword,
   updateProfile,
 } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { MdOutlineError, MdThumbUp } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/GH-logo.png";
 import "../css/ProfilePage.css";
 import { auth } from "../firebase";
 
+interface FormValues {
+  email: string;
+  name: string;
+  newPassword: string;
+  currentPassword: string;
+}
+
 function ProfilePage() {
   const [user, setUser] = useState<User | null>(null);
-  const [newUsername, setNewUsername] = useState("");
-  const [currentPassword, setCurrentPassword] = useState("");
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [error, setError] = useState<string | null>(null);
+
+  const form = useForm<FormValues>({
+    initialValues: {
+      email: "",
+      name: "",
+      newPassword: "",
+      currentPassword: "",
+    },
+
+    validate: {
+      name: (val) =>
+        val.length < 2 ? "Username should include at least 2 characters" : null,
+      email: (val) =>
+        /^\S+@\S+$/.test(val)
+          ? null
+          : "Make sure you are using mail@mail.com format",
+      newPassword: (val) =>
+        val.length < 7 ? "Password should include at least 7 characters" : null,
+      currentPassword: (val) =>
+        val.length === 0 ? "Please enter your current password." : null,
+    },
+  });
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser: User | null) => {
       setUser(authUser);
       if (authUser && authUser.displayName) {
-        setNewUsername(authUser.displayName);
+        form.values.name = authUser.displayName;
       }
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [form]);
 
   const handleSaveChanges = async () => {
     setError(null);
 
     try {
-      if (!currentPassword) {
+      if (!form.values.currentPassword) {
         setError("Please enter your current password.");
         return;
       }
@@ -52,20 +82,39 @@ function ProfilePage() {
         console.log("Attempting to reauthenticate...");
         const credential = EmailAuthProvider.credential(
           user.email,
-          currentPassword
+          form.values.currentPassword
         );
 
         await reauthenticateWithCredential(user, credential);
 
         console.log("Reauthentication successful. Updating profile...");
         await updateProfile(user, {
-          displayName: newUsername || user.displayName,
+          displayName: form.values.name || user.displayName,
+        });
+
+        if (form.values.newPassword) {
+          console.log("Updating password...");
+          await updatePassword(user, form.values.newPassword);
+          console.log("Password updated successfully!");
+        }
+
+        showNotification({
+          title: "Profile updated successfully!",
+          message: "Your profile has been updated successfully.",
+          color: "green",
+          icon: <MdThumbUp />,
         });
 
         console.log("Profile updated successfully!");
       }
     } catch (error) {
       setError("Failed to update profile. Please check your current password");
+      showNotification({
+        title: "Failed to update profile!",
+        message: "Please check your current password.",
+        color: "red",
+        icon: <MdOutlineError />,
+      });
       console.error(error);
     }
   };
@@ -110,20 +159,36 @@ function ProfilePage() {
                 <TextInput label="Email" value={user?.email || ""} disabled />
                 <TextInput
                   label="Username"
-                  value={newUsername}
+                  placeholder="Your name"
+                  value={form.values.name}
                   onChange={(event) =>
-                    setNewUsername(event.currentTarget.value)
+                    form.setFieldValue("name", event.currentTarget.value)
                   }
+                  error={form.errors.name}
+                  radius="md"
                 />
-                <PasswordInput label="New Password" placeholder="●●●●●●●●●●" />
                 <PasswordInput
-                  required
+                  label="New Password"
+                  placeholder="●●●●●●●●●●"
+                  value={form.values.newPassword}
+                  onChange={(event) =>
+                    form.setFieldValue("newPassword", event.currentTarget.value)
+                  }
+                  error={form.errors.newPassword}
+                  radius="md"
+                />
+                <PasswordInput
                   label="Current Password"
                   placeholder="●●●●●●●●●●"
-                  value={currentPassword}
+                  value={form.values.currentPassword}
                   onChange={(event) =>
-                    setCurrentPassword(event.currentTarget.value)
+                    form.setFieldValue(
+                      "currentPassword",
+                      event.currentTarget.value
+                    )
                   }
+                  error={form.errors.currentPassword}
+                  radius="md"
                 />
 
                 <Button className="button-style" fullWidth type="submit">

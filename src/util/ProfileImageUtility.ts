50b4f07f864d3943b7d9dc4b8b3c54c069884fc6
Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
2024-01-18 10:58:54 +0100

    remove unused thumb images and update default profileimage

--- src/util/ProfileImageUtility.ts
+++ src/util/ProfileImageUtility.ts
@@ -1,8 +1,8 @@
-import logo from "../assets/GH-logo.png";
+import defaultProfile from "../assets/defaultProfile.jpg";
 import profile1 from "../assets/profile1.jpg";
 import profile2 from "../assets/profile2.jpg";
 
-export const profileImages = [logo, profile1, profile2];
+export const profileImages = [defaultProfile, profile1, profile2];
 
 export const getProfileImage = (index: number): string => {
   return profileImages[index];
